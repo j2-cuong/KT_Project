b1f0bc04002566d36c154195be2ef8d3f7965e0b
CuongNH <j2.cuong@gmail.com>
2024-04-14 02:39:47 +0700

    First Commit

---
+++ src/data/team-data.js
@@ -0,0 +1,24 @@
+export const teamData = [
+  {
+    img: "/img/team-1.png",
+    name: "Hậu Mõm",
+    position: "Web Developer",
+  },
+  {
+    img: "/img/team-2.png",
+    name: "Nguyễn Hữu Cường",
+    position: "Developer",
+  },
+  {
+    img: "/img/team-3.jpg",
+    name: "Phan Văn Đoàn",
+    position: "Developer",
+  },
+  {
+    img: "/img/team-4.jpg",
+    name: "Nguyễn Minh Đức",
+    position: "Developer",
+  },
+];
+
+export default teamData;
